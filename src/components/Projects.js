dd0947bf53b4c1a99bfbc0bb461438929cbb0f5b
Harlei Melo <harleimelo0@gmail.com>
2025-02-05 17:29:46 -0300

    feat: :art: Adição dos links tanto para deploy e github

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,11 @@
-import React, { useState, useRef } from 'react' 
+import React, { useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPersonCircleQuestion, faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
+import { 
+  faPersonCircleQuestion, 
+  faEarthAmericas, 
+  faExternalLinkAlt 
+} from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import CustomHook from './CustomHook';
 
 function Projects() {
@@ -10,21 +15,27 @@ function Projects() {
       des: 'Projeto desenvolvido em React.js e CSS para evidenciar minhas habilidades na criação de interfaces modernas e responsivas.',
       mission: 'Desenvolvimento Front-end',
       language: 'React.js, CSS',
-      images: '/project-leh.png'
+      images: '/project-leh.png',
+      deploy: 'https://leticiacs.com.br',
+      github: 'https://github.com/Harlei-Melo/Leticia_Portifolio'
     },
     {
       name: 'Projeto Nubank',
       des: 'Projeto realizado em HTML, SASS e JavaScript, com o objetivo de reinventar o design original e testar minhas técnicas de desenvolvimento web.',
       mission: 'Redesign e Desenvolvimento Front-end',
       language: 'HTML, SASS, JavaScript',
-      images: '/project-nubank.png'
+      images: '/project-nubank.png',
+      deploy: 'https://projeto-nubank.netlify.app',
+      github: 'https://github.com/Harlei-Melo/Projeto-Nubank'
     },
     {
       name: 'Projeto Card Glass',
       des: 'Projeto inovador desenvolvido em HTML e CSS, utilizando o efeito glass para criar um design moderno e sofisticado que ressalta minha criatividade.',
       mission: 'Design e Desenvolvimento Front-end',
       language: 'HTML, CSS',
-      images: '/project-glass.png'
+      images: '/project-glass.png',
+      deploy: 'https://projeto-card-js.netlify.app',
+      github: 'https://github.com/Harlei-Melo/Card-JS'
     }
   ]);
 
@@ -66,6 +77,26 @@ function Projects() {
                    <h4>Tecnologias</h4>
                    <div className="des">{value.language}</div>
                  </div>
+               </div>
+               <div className="project-links">
+                 <a 
+                   href={value.deploy} 
+                   target="_blank" 
+                   rel="noopener noreferrer" 
+                   className="btn"
+                 >
+                   <FontAwesomeIcon icon={faExternalLinkAlt} className="icon" />
+                   <span>Deploy</span>
+                 </a>
+                 <a 
+                   href={value.github} 
+                   target="_blank" 
+                   rel="noopener noreferrer" 
+                   className="btn"
+                 >
+                   <FontAwesomeIcon icon={faGithub} className="icon" />
+                   <span>GitHub</span>
+                 </a>
                </div>
              </div>
            </div>
