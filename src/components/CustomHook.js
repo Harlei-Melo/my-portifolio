82f5f7af7a76ff43a2d559bab3b4b93f5d9bb6f7
Harlei Melo <harleimelo0@gmail.com>
2025-02-05 17:10:22 -0300

    fix: :bug: Foi corrigido o custom hook

--- src/components/CustomHook.js
+++ src/components/CustomHook.js
@@ -1,34 +1,38 @@
-import { useEffect } from 'react' 
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 
 const CustomHook = (refTab = null, refList = null) => {
-const scrollTab = refTab;
-const divs = refList;
-const activeTab = useSelector(state => state.activeTab);
+  const scrollTab = refTab;
+  const divs = refList;
+  const activeTab = useSelector(state => state.activeTab);
 
-useEffect(() => {
-    if(scrollTab.current.classList.contains(activeTab)){
-        const componentNode = scrollTab.current;
-        componentNode.scrollIntoView({ behavior: 'smooth' });
+  useEffect(() => {
+    if (scrollTab.current.classList.contains(activeTab)){
+      const componentNode = scrollTab.current;
+      componentNode.scrollIntoView({ behavior: 'smooth' });
     }
-    if(divs !== null){
+    if (divs !== null){
+      divs.current.forEach((div) => {
+        div.classList.add('animation');
+      });
+      const handlScroll = () => {
+        const scrollPosition = window.scrollY;
         divs.current.forEach((div) => {
-            div.classList.add('animation');
-          });
-        const handlScroll = () => {
-          const scrollPosition = window.scrollY;
-          divs.current.forEach((div) => {
-            const offsetTop = div.getBoundingClientRect().top + scrollPosition;
-            if (scrollPosition >= offsetTop - (window.innerHeight / (1.5))) {
-              div.classList.add('active');
-            }else{
-              div.classList.remove('active');
-            }
-          });
-        }
-          window.addEventListener('scroll', handlScroll);
+          const offsetTop = div.getBoundingClientRect().top + scrollPosition;
+          if (scrollPosition >= offsetTop - (window.innerHeight / 1.5)) {
+            div.classList.add('active');
+          } else {
+            div.classList.remove('active');
+          }
+        });
+      }
+      window.addEventListener('scroll', handlScroll);
+      // Opcional: limpar o event listener ao desmontar o componente
+      return () => {
+        window.removeEventListener('scroll', handlScroll);
+      }
     }
-}, [activeTab])
+  }, [activeTab, scrollTab, divs]);  // Adicione scrollTab e divs aqui
 }
 
-export default CustomHook
+export default CustomHook;
